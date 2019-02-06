ea877b1ec81809d4b038f55328ba679399330841
Imon Ovbude <imonovbude@gmail.com>
2019-02-06 12:32:07 -0800

    setup action creator

--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -3,6 +3,7 @@ const initialState = {
   characters: []
   // Array characters, Boolean fetching, null error.
 };
+
 export const charsReducer = (state = initialState, action) => {
   switch (action.type) {
     // Fill me in with the important reducers
